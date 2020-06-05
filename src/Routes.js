7ec4f20c0aa177dad41db19b99b9ce652443d9d0
schettn <nicoschett@icloud.com>
2020-06-05 12:35:03 +0200

    Add front-legacy
    The front-legacy repository was archived and the code
    was moved to this repository, to create a clear cute for
    the next step of the project.

---
+++ src/Routes.js
@@ -0,0 +1,85 @@
+//#region > Imports
+//> React
+// Contains all the functionality necessary to define React components
+import React from "react";
+// DOM bindings for React Router
+import { Route, Switch } from "react-router-dom";
+
+//> Components
+import {
+  HomePage,
+  ProfilePage,
+  MessagePage,
+  RedirectPage,
+  SearchPage,
+  CompanyPage,
+  TalkPage,
+} from "./components/pages";
+//#endregion
+
+//#region > Components
+class Routes extends React.Component {
+  render() {
+    const { globalState, globalFunctions } = this.props;
+
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={(props) => (
+            <HomePage
+              globalFunctions={globalFunctions}
+              globalState={globalState}
+              {...props}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/redirect"
+          render={() => {
+            // Get name of window which was set by the parent to be the unique
+            // request key
+            const requestKey = window.name;
+            // Update corresponding entry with the redirected url which should
+            // contain either access token or failure reason in the query
+            // parameter / hash
+            localStorage.setItem(requestKey, window.location.href);
+            window.close();
+          }}
+        />
+        <Route
+          exact
+          path="/u/:username"
+          component={(props) => (
+            <ProfilePage
+              globalFunctions={globalFunctions}
+              globalState={globalState}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/c/:name"
+          component={(props) => (
+            <CompanyPage
+              globalFunctions={globalFunctions}
+              globalState={globalState}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
+}
+//#endregion
+
+//#region > Exports
+export default Routes;
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
---
+++ src/components/molecules/index.js
@@ -0,0 +1,17 @@
+//#region > Imports
+//> Components
+// Import all components to export them for easy access from parent components
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import UserActionCard from "./UserActionCard";
+//#endregion
+
+//#region > Exports
+// Export the components for easy access from parent components
+export { Footer, Navbar, UserActionCard };
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
